22480be04ffa3e6628fc85dbf0ea0137966c0f48
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2021-10-09 23:17:54 -0400

    coroutine example

--- src/vs2022/coro01/coro_ex01/coro_ex01.cpp
+++ src/vs2022/coro01/coro_ex01/coro_ex01.cpp
@@ -1,18 +1,49 @@
 // coro_ex01.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <concepts>
 #include <coroutine>
+#include <exception>
+#include <iostream>
 
-// lazy generator
+struct ReturnObject {
+	struct promise_type {
+		ReturnObject get_return_object() { return {}; }
+		std::suspend_never initial_suspend() { return {}; }
+		std::suspend_never final_suspend() noexcept { return {}; }
+		void unhandled_exception() {}
+		void return_void() {};
+	};
+};
 
-generator<int> generatorForNumbers(int begin, int inc = 1) {
-    for (int i = begin;; i += inc) {
-        co_yield i;
-    }
+struct Awaiter {
+	std::coroutine_handle<>* hp_;
+	constexpr bool await_ready() const noexcept { return false; }
+	void await_suspend(std::coroutine_handle<> h) { *hp_ = h; }
+	constexpr void await_resume() const noexcept {}
+};
+
+ReturnObject 
+counter(std::coroutine_handle<>* continuation_out)
+{
+	Awaiter a{ continuation_out };
+	for (unsigned i = 0;; ++i) {
+		co_await a;
+		std::cout << "counter: " << i << std::endl;
+	}
 }
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void main1() {
+	std::coroutine_handle<> h;
+	counter(&h);
+	for (int i = 0; i < 3; ++i) {
+		std::cout << "In main1 function\n";
+		h();
+	}
+	h.destroy();
+}
+
+int main(const int argc, const char* argv[]) {
+	main1();
+	return 0;
 }
